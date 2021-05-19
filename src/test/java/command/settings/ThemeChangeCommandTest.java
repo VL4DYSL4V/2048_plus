3f490a2ce0738b05f5bbba014747a967240038ad
VladislavArzamastsev <68342736+VladislavArzamastsev@users.noreply.github.com>
2021-05-19 18:11:56 +0300

    Written tests for Theme change command (closes #50)

---
+++ src/test/java/command/settings/ThemeChangeCommandTest.java
@@ -0,0 +1,62 @@
+package command.settings;
+
+import dao.preferences.PreferencesDAO;
+import handler.CommandHandler;
+import mock.ThisThreadCommandHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import preferences.UserPreferences;
+import testUtils.ThemeUtils;
+import testUtils.UserPreferencesUtils;
+import view.theme.Theme;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class ThemeChangeCommandTest {
+
+    private CommandHandler commandHandler;
+    private UserPreferences userPreferences;
+    private PreferencesDAO preferencesDAO;
+    private ThemeChangeCommand themeChangeCommand;
+
+    @BeforeEach
+    void init(){
+        userPreferences = spy(UserPreferencesUtils.getDefaultUserPreferences());
+        commandHandler = spy(new ThisThreadCommandHandler());
+        preferencesDAO = mock(PreferencesDAO.class);
+        themeChangeCommand = new ThemeChangeCommand(commandHandler, userPreferences, preferencesDAO);
+    }
+
+    @Test
+    void testNullAsParam() {
+        assertThrows(NullPointerException.class, () -> themeChangeCommand.setParam(null));
+    }
+
+    @Test
+    void executeInCommandHandler() {
+        themeChangeCommand.execute();
+        verify(commandHandler).execute(any());
+    }
+
+    @Test
+    void setNewTheme(){
+        Theme theme = ThemeUtils.getRandomTheme();
+        themeChangeCommand.setParam(theme);
+        themeChangeCommand.execute();
+        verify(userPreferences).setTheme(theme);
+        verify(preferencesDAO).saveOrUpdate(userPreferences);
+    }
+
+    @Test
+    void setSameTheme(){
+        Theme theme = userPreferences.getTheme();
+        themeChangeCommand.setParam(theme);
+        themeChangeCommand.execute();
+        verify(userPreferences).setTheme(theme);
+        verify(preferencesDAO, times(0)).saveOrUpdate(userPreferences);
+    }
+}
